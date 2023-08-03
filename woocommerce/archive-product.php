79435c04c4023f42a369c5ec503730dfb8f50b3a
Dimasik <dimakadenuk2000@gmail.com>
2023-08-03 16:06:00 +0300

    + фильтр

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -29,22 +29,7 @@ get_header( 'shop' );
 do_action( 'woocommerce_before_main_content' );
 
 ?>
-<header class="woocommerce-products-header">
-	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
-		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
-	<?php endif; ?>
-
-	<?php
-	/**
-	 * Hook: woocommerce_archive_description.
-	 *
-	 * @hooked woocommerce_taxonomy_archive_description - 10
-	 * @hooked woocommerce_product_archive_description - 10
-	 */
-	do_action( 'woocommerce_archive_description' );
-	?>
-</header>
-<h1>Archive product php</h1>
+<?php echo do_shortcode('[pwf_filter id="291"]'); ?>
 <?php
 if ( woocommerce_product_loop() ) {
 
